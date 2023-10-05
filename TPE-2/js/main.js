d248a523dfef95b660c7d810207da5a78a8d117c
Diego Lopez <lopezdiegomartin5@gmail.com>
2023-10-04 21:00:33 -0300

    Modifique login y register

--- TPE-2/js/main.js
+++ TPE-2/js/main.js
@@ -9,6 +9,48 @@ addEventListener("DOMContentLoaded", (e) => {
         containerSitio.classList.add('activarSitio')
     }, 5000);
 
+    // ------- Formulario registro login-------
+
+    let formLogin = document.querySelector("#login-formulario");
+    let formRegistro = document.querySelector("#registro-formulario");
+
+    document.getElementById('btn-login').addEventListener('click', function(){
+        if(formLogin.classList.contains("ocultar")){
+            formLogin.classList.remove("ocultar");
+            formLogin.classList.add("activar");
+            formRegistro.classList.remove("activar");
+            formRegistro.classList.add("ocultar");
+        }else if(formLogin.classList.contains("activar")){
+            formLogin.classList.remove("activar");
+            formLogin.classList.add("ocultar");
+            formRegistro.classList.remove("ocultar");
+            formRegistro.classList.add("activar");
+        }else{
+            formLogin.classList.add("activar");
+            formRegistro.classList.remove("activar");
+            formRegistro.classList.add("ocultar");
+        }
+    });
+    
+    document.getElementById('btn-registro').addEventListener('click', function(){
+        if(formRegistro.classList.contains("ocultar")){
+            formRegistro.classList.remove("ocultar");
+            formRegistro.classList.add("activar");
+            formLogin.classList.remove("activar");
+            formLogin.classList.add("ocultar");
+        }else if(formRegistro.classList.contains("activar")){
+            formRegistro.classList.remove("activar");
+            formRegistro.classList.add("ocultar");
+            formLogin.classList.remove("ocultar");
+            formLogin.classList.add("activar");
+        }else{
+            formRegistro.classList.add("ocultar");
+            formLogin.classList.remove("ocultar");
+            formLogin.classList.add("activar");
+
+        }
+    });
+
 
     // ------ Menu y carrito-------
     document.querySelector(".btnMenu").addEventListener("click", btnToggle);
