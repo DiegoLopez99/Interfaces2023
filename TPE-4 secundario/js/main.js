a5c33cdc8c15f0cef86035edbeadd30d24a2b538
Martin <acostamartin1270@gmail.com>
2023-11-25 18:15:13 -0300

    Animaciones 3d cards, navbar sticky

--- TPE-4 secundario/js/main.js
+++ TPE-4 secundario/js/main.js
@@ -21,12 +21,18 @@ addEventListener("DOMContentLoaded", (e) => {
         let personajeDefault = document.querySelector("#personajeDefault");
         let personajeNegro = document.querySelector("#personajeNegros");
         let duende = document.querySelector("#img-duende");
+        let gamecard1 = document.querySelector('.gamecard-1');
+        let gamecard2 = document.querySelector('.gamecard-2');
+        let gamecard3 = document.querySelector('.gamecard-3');
 
         personajeDefault.style.bottom = posicion * 0.1 + "px";
         personajeNegro.style.bottom = posicion * 0.3 + "px";
         personajeRosa.style.top = posicion * 0.1 + "px";
         duende.style.bottom = posicion * 0.15 + "px";
         
+        gamecard1.style.top = posicion * -0.03 + "px";
+        gamecard2.style.top = posicion * -0.15 + "px";
+        gamecard3.style.top = posicion * -0.3 + "px";
     });
 
     document.querySelector("#seccionParallaxMouseMove").addEventListener('mousemove', e=>{
@@ -72,4 +78,52 @@ addEventListener("DOMContentLoaded", (e) => {
 
     observerCards.observe(document.querySelector(".section-3"))
 
+
+    function callback(entries){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                console.log("funciona")
+                activarLogoNavbar(logoNavbar);
+                activarNavbarOnScroll(navbar);
+            }
+            else{
+                desactivarLogoNavbar(logoNavbar);
+                desactivarNavbarOnScroll(navbar);
+            }
+        });
+    }
+    
+    function activarNavbarOnScroll(entry){
+        entry.classList.toggle('activar');
+    }
+
+    function activarLogoNavbar(entry){
+        entry.classList.add('activarImg');
+    }
+
+    function desactivarLogoNavbar(entry){
+        entry.classList.remove('activarImg')
+        entry.classList.add('desactivarImg')
+    }
+
+    function desactivarNavbarOnScroll(entry){
+        entry.classList.remove('activar')
+    }
+
+
+    const options = {
+        root: null,
+        rootMargin:'50px' ,
+        threshold : 0
+    }
+    
+    const observer = new IntersectionObserver(callback, options);
+
+    const navbar = document.querySelector('.navbar');
+    const logoNavbar = document.querySelector('#titulo-logo');
+    const element = document.querySelector('#logo-central')
+    const header = document.querySelector('header-nav')
+    
+    observer.observe(element);
+    
 });
